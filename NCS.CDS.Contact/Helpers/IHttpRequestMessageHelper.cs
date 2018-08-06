10b1cb0a4ecd76be21e539b65b7a43e17c1e7668
K-Bains <kailash.bains@ksbtechnologies.co.uk>
2018-08-06 13:44:55 +0100

    Update Touchpoint Id to string from guid

--- NCS.CDS.Contact/Helpers/IHttpRequestMessageHelper.cs
+++ NCS.CDS.Contact/Helpers/IHttpRequestMessageHelper.cs
@@ -7,6 +7,6 @@ namespace NCS.DSS.Contact.Helpers
     public interface IHttpRequestMessageHelper
     {
         Task<T> GetContactDetailsFromRequest<T>(HttpRequestMessage req);
-        Guid? GetTouchpointId(HttpRequestMessage req);
+        string GetTouchpointId(HttpRequestMessage req);
     }
 }
--- NCS.DSS.Contact.Tests/GetContactHttpTriggerTests.cs
+++ NCS.DSS.Contact.Tests/GetContactHttpTriggerTests.cs
@@ -38,13 +38,13 @@ namespace NCS.DSS.Contact.Tests
             _resourceHelper = Substitute.For<IResourceHelper>();
             _httpRequestMessageHelper = Substitute.For<IHttpRequestMessageHelper>();
             _getContactHttpTriggerService = Substitute.For<IGetContactHttpTriggerService>();
-            _httpRequestMessageHelper.GetTouchpointId(_request).Returns(new Guid());
+            _httpRequestMessageHelper.GetTouchpointId(_request).Returns("0000000001");
         }
 
         [Test]
         public async Task GetContactHttpTrigger_ReturnsStatusCodeBadRequest_WhenTouchpointIdIsNotProvided()
         {
-            _httpRequestMessageHelper.GetTouchpointId(_request).Returns((Guid?)null);
+            _httpRequestMessageHelper.GetTouchpointId(_request).Returns((string)null);
 
             // Act
             var result = await RunFunction(ValidCustomerId);
