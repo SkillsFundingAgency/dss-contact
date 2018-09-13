b488b0d663fd4ddec5732848d28a82bba21893f1
Anil Dhiman <anil.dhiman@outlook.com>
2018-09-13 14:23:31 +0100

    Added valid Regex to mobile & landline numbers

--- NCS.CDS.Contact/Models/ContactDetailsPatch.cs
+++ NCS.CDS.Contact/Models/ContactDetailsPatch.cs
@@ -18,15 +18,22 @@ namespace NCS.DSS.Contact.Models
         public PreferredContactMethod? PreferredContactMethod { get; set; }
 
         [StringLength(20)]
-        [Example(Description = "0777 777777")]
+        [Example(Description = "UK mobile phone number with optional +44 national code, also allows optional brackets and spaces at appropriate positions e.g.   07222 555555   or   (07222) 555555   or   +44 7222 555 555")]
+        [RegularExpression(@"^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$)")]
         public string MobileNumber { get; set; }
 
         [StringLength(20)]
-        [Example(Description = "0121 888777")]
+        [Example(Description = "UK phone number. Allows 3, 4 or 5 digit regional prefix, with 8, 7 or 6 digit phone number respectively, plus optional 3 or 4 digit extension number prefixed with a # symbol. " +
+                                "Also allows optional brackets surrounding the regional prefix and optional spaces between appropriate groups of numbers    e.g.   " +
+                                "01222 555 555   or   (010) 55555555 #2222   or   0122 555 5555#222")]
+        [RegularExpression(@"^((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$")]
         public string HomeNumber { get; set; }
 
         [StringLength(20)]
-        [Example(Description = "0121 444889")]
+        [Example(Description = "Alternative UK phone number. Allows 3, 4 or 5 digit regional prefix, with 8, 7 or 6 digit phone number respectively, plus optional 3 or 4 digit extension number prefixed with a # symbol. " +
+                                "Also allows optional brackets surrounding the regional prefix and optional spaces between appropriate groups of numbers    e.g.   " +
+                                "01222 555 555   or   (010) 55555555 #2222   or   0122 555 5555#222")]
+        [RegularExpression(@"^((\(?0\d{4}\)?\s?\d{3}\s?\d{3})|(\(?0\d{3}\)?\s?\d{3}\s?\d{4})|(\(?0\d{2}\)?\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$")]
         public string AlternativeNumber { get; set; }
 
         [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$")]
