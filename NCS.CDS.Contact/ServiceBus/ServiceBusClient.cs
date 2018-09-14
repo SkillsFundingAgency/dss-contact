a2d0b0126c853918e09ab6ff9eeae66596cbf695
Anil Dhiman <anil.dhiman@outlook.com>
2018-09-14 10:57:05 +0100

    Remove target touchpoint ID

--- NCS.CDS.Contact/ServiceBus/ServiceBusClient.cs
+++ NCS.CDS.Contact/ServiceBus/ServiceBusClient.cs
@@ -29,8 +29,7 @@ namespace NCS.DSS.Contact.ServiceBus
                 LastModifiedDate = contactDetails.LastModifiedDate,
                 URL = reqUrl + "/" + contactDetails.ContactId,
                 IsNewCustomer = false,
-                TouchpointId = contactDetails.LastModifiedTouchpointId,
-                TargetIdTransfer = ""
+                TouchpointId = contactDetails.LastModifiedTouchpointId
             };
 
             var msg = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageModel))))
@@ -55,8 +54,7 @@ namespace NCS.DSS.Contact.ServiceBus
                 LastModifiedDate = contactDetails.LastModifiedDate,
                 URL = reqUrl,
                 IsNewCustomer = false,
-                TouchpointId = contactDetails.LastModifiedTouchpointId,
-                TargetIdTransfer = ""
+                TouchpointId = contactDetails.LastModifiedTouchpointId
             };
 
             var msg = new BrokeredMessage(new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageModel))))
@@ -79,7 +77,6 @@ namespace NCS.DSS.Contact.ServiceBus
         public string URL { get; set; }
         public bool IsNewCustomer { get; set; }
         public string TouchpointId { get; set; }
-        public string TargetIdTransfer { get; set; }
     }
 
 }
